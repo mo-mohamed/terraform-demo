d5d5ddb6ebae474649abac3eb765ae809d5288e5
Craig Dunn <craig@Craigs-MacBook-Pro.local>
2023-07-12 11:30:06 +0200

    rework

--- instance.tf
+++ instance.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "ec2_instance" {
   subnet_id = var.subnet
   instance_type = var.instance_type
   key_name = var.aws_keypair
+  count = 5
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
 }
 
--- network.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = "192.168.0.0/16"
-}
