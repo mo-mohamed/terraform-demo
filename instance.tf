bf6df60472f89210ed12f751626fc313478331aa
Mostafa Mohamed <mostafamohamed@Mostafas-MacBook-Pro.local>
2025-01-15 08:27:01 +0300

    changes

--- instance.tf
+++ instance.tf
@@ -1,11 +1,8 @@
 
 resource "aws_instance" "ec2_instance" {
   ami = var.ami_id
-  subnet_id = var.subnet
   instance_type = var.instance_type
-  key_name = var.aws_keypair
-  count = 5
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+  count = 2
 }
 
 
